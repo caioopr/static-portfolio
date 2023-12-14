2ab021bf9e1524fcb1a45030d182d9d807d3a9d0
Caio Prado <caioppubli@gmail.com>
2023-12-13 21:13:42 -0300

    project card fix

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,7 +6,7 @@ export default function ProjectCard(post: ProjectInterface) {
       key={post.name}
       className="flex flex-col items-start justify-between"
     >
-      <div className="group relative border border-gray-300 p-4 dark:border-neutral-700">
+      <div className="group w-full h-full relative border border-gray-300 p-4 dark:border-neutral-700">
         <h3 className="mt-1.5 text-md font-semibold leading-6 group-hover:text-gray-700">
           <a href={post.repository}>
             <span className="absolute inset-0" />
@@ -16,16 +16,14 @@ export default function ProjectCard(post: ProjectInterface) {
         <div className="flex flex-wrap mt-1.5 items-center gap-x-2 text-xs">
           {post.tech_stack?.map((stack) => (
             <p
-              key={post.name}
+              key={post.name + stack.tech_name}
               className="relative z-10 mb-1 border border-gray-300 rounded-sm  px-1.5 py-1.5 font-medium  hover:bg-gray-100"
             >
               {stack.tech_name}
             </p>
           ))}
         </div>
-        <p className="mt-1.5 line-clamp-3 text-sm leading-6 text-gray-700">
-          {post.description}
-        </p>
+        <p className="mt-1.5  text-sm  text-gray-700">{post.description}</p>
       </div>
     </article>
   );
--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -36,50 +36,48 @@ export const projects: ProjectInterface[] = [
         tech_name: "Docker",
       }
     ],
-    description:"A display and management system for products stock that helps the customer and the store owner to keep track of product availability and cost",
+    description:"A display and management system for products stock that helps the customer and the store owner to keep track of product availability and cost.",
     repository: "https://github.com/caioopr/products-display"
   },
   {
-    name:"Project",
+    name:"AppPointment",
     tech_stack: [
       {
         tech_name: "TypeScript",
       },
       {
+        tech_name: "Node.js",
+      },
+      {
+        tech_name: "Nest.js",
+      },
+      {
         tech_name: "Vue.js",
-      },
-      {
-        tech_name: "Java",
-
-      },
-      {
-        tech_name: "Spring Boot",
       },
       {
         tech_name: "PostgreSQL",
       },
       {
         tech_name: "Docker",
+      },
+      {
+        tech_name: "Kubernetes",
       }
     ],
-    description:"A display and management system for products stock that helps the customer and the store owner to keep track of product availability and cost",
-    repository: "https://github.com/caioopr/products-display"
+    description:"An easy way of scheduling and manage an appointment for both client and consultant.",
+    repository: "https://github.com/caioopr/APPpointment"
   },
-   {
-    name:"Projecttttt",
+  {
+    name:"Epet",
     tech_stack: [
       {
-        tech_name: "TypeScript",
-      },
-      {
-        tech_name: "Vue.js",
-      },
-      {
         tech_name: "Java",
-
       },
       {
         tech_name: "Spring Boot",
+      },
+      {
+        tech_name: "Thymeleaf",
       },
       {
         tech_name: "PostgreSQL",
@@ -88,7 +86,26 @@ export const projects: ProjectInterface[] = [
         tech_name: "Docker",
       }
     ],
-    description:"A display and management system for products stock that helps the customer and the store owner to keep track of product availability and cost",
-    repository: "https://github.com/caioopr/products-display"
+    description:"A petshot online system",
+    repository: "https://github.com/caioopr/APPpointment" //TODO
+  },
+  {
+    name:"Hare",
+    tech_stack: [
+      {
+        tech_name: "Kotlin",
+      },
+      {
+        tech_name: "Android",
+      },
+      {
+        tech_name: "Firebase",
+      },
+      {
+        tech_name: "SQLite",
+      }
+    ],
+    description:"Keep up your habits and create new ones.",
+    repository: "https://github.com/caioopr/APPpointment" //TODO
   }
 ]
